anishathalye/notary

commit fc8dc0d4e302a478d4d05191a8080c4d3b22c287

Add command-line arguments to make demos easier

verify.rkt
@@ -4,23 +4,26 @@
          yosys/parameters
          shiva
          rosutil
-         (only-in racket struct-copy)
+         (only-in racket struct-copy make-parameter string->number exit)
+         (only-in racket/cmdline command-line)
          syntax/parse/define)
+
+(define DEFAULT-TRY-VERIFY-AFTER 180340)
 
 (overapproximate-symbolic-load-threshold 64)
 (overapproximate-symbolic-store-threshold 64)
 
 (define (input-setter s)
   (struct-copy soc_s s
-                      [resetn #t]
-                      [gpio_pin_in (bv 0 8)]
-                      [uart_rx (bv #b1111 4)]))
+               [resetn #t]
+               [gpio_pin_in (bv 0 8)]
+               [uart_rx (bv #b1111 4)]))
 
 (define (init-input-setter s)
   (struct-copy soc_s s
-                      [resetn #f]
-                      [gpio_pin_in (bv 0 8)]
-                      [uart_rx (bv #b1111 4)]))
+               [resetn #f]
+               [gpio_pin_in (bv 0 8)]
+               [uart_rx (bv #b1111 4)]))
 
 (define (statics s)
   ; for some reason, the picorv32 has a physical register for x0/zero,
@@ -39,15 +42,36 @@
       ; leave it untouched on all cycles past the 1st
       #f))
 
-(verify-deterministic-start
- soc_s
- new-symbolic-soc_s
- #:invariant soc_i
- #:step soc_t
- #:init-input-setter init-input-setter
- #:input-setter input-setter
- #:state-getters (append registers memories)
- #:statics statics
- #:overapproximate overapproximate
- #:print-style 'names
- #:try-verify-after 180340)
+(define start (make-parameter DEFAULT-TRY-VERIFY-AFTER))
+(define limit (make-parameter #f))
+(define exactly (make-parameter #f))
+
+(command-line
+ #:program "verify"
+ #:once-each
+ [("-s" "--start") s
+                   "Start invoking the SMT solver beyond this point"
+                   (start (string->number s))]
+ [("-l" "--limit") l
+                   "Limit number of cycles to try"
+                   (limit (string->number l))]
+ [("-x" "--exactly") x
+                     "Run for exactly this many cycles and then try to verify"
+                     (exactly (string->number x))])
+
+(define cycles
+  (verify-deterministic-start
+   soc_s
+   new-symbolic-soc_s
+   #:invariant soc_i
+   #:step soc_t
+   #:init-input-setter init-input-setter
+   #:input-setter input-setter
+   #:state-getters (append registers memories)
+   #:statics statics
+   #:overapproximate overapproximate
+   #:print-style 'names
+   #:try-verify-after (or (exactly) (start))
+   #:limit (or (exactly) (limit))))
+
+(exit (if cycles 0 1))

Makefile
@@ -27,7 +27,7 @@ LDFLAGS := -melf32lriscv -nostdlib
 BIN2COEFLAGS := --width 32 --depth $(ROM_DEPTH) --fill 0
 
 .PHONY: all
-all: soc.rkt
+all: soc.rkt verify
 
 .PHONY: clean
 clean:
@@ -70,4 +70,3 @@ soc.rkt: hw/soc.smt2
 .PHONY: verify
 verify: soc.rkt verify.rkt
 	raco make verify.rkt
-	racket verify.rkt

commit b443a0fadccd50c1d9ac4cab9e25935916f83caf

Fix CI to actually run verification
This was broken in fc8dc0d4e302a478d4d05191a8080c4d3b22c287, which
changed the Makefile to only compile but not run the verification
program.

.travis.yml
@@ -33,9 +33,9 @@ before_install:
   - raco pkg install --no-docs --batch --auto https://github.com/anishathalye/rtl.git
 
 script:
-  - make
+  - make verify
   # we can't print all the output, because it exceeds the Travis limit
   #
   # and we can't redirect all output to /dev/null or take the tail of the
   # output, because then Travis detects it as a stalled build
-  - make verify | awk 'BEGIN { printall=0 } $1 == "cycle" && $2 % 1000 == 0 || printall { print } $1 == "cycle" && $2 == "180000" { printall=1 }'
+  - racket verify.rkt | awk 'BEGIN { printall=0 } $1 == "cycle" && $2 % 1000 == 0 || printall { print } $1 == "cycle" && $2 == "180000" { printall=1 }'

fw/firmware.s
@@ -44,22 +44,22 @@ _reset:
     la t0, _ssram /* 0x20000000 */
     la t1, _esram /* 0x20020000 */
 _loop:
-    sw zero,0x00(t0)
-    sw zero,0x04(t0)
-    sw zero,0x08(t0)
-    sw zero,0x0c(t0)
-    sw zero,0x10(t0)
-    sw zero,0x14(t0)
-    sw zero,0x18(t0)
-    sw zero,0x1c(t0)
-    sw zero,0x20(t0)
-    sw zero,0x24(t0)
-    sw zero,0x28(t0)
-    sw zero,0x2c(t0)
-    sw zero,0x30(t0)
-    sw zero,0x34(t0)
-    sw zero,0x38(t0)
-    sw zero,0x3c(t0)
+    sw zero, 0x00(t0)
+    sw zero, 0x04(t0)
+    sw zero, 0x08(t0)
+    sw zero, 0x0c(t0)
+    sw zero, 0x10(t0)
+    sw zero, 0x14(t0)
+    sw zero, 0x18(t0)
+    sw zero, 0x1c(t0)
+    sw zero, 0x20(t0)
+    sw zero, 0x24(t0)
+    sw zero, 0x28(t0)
+    sw zero, 0x2c(t0)
+    sw zero, 0x30(t0)
+    sw zero, 0x34(t0)
+    sw zero, 0x38(t0)
+    sw zero, 0x3c(t0)
     addi t0, t0, 0x40
     bne t0, t1, _loop
 /* done with deterministic start,

verify.rkt
@@ -47,7 +47,6 @@
 (define exactly (make-parameter #f))
 
 (command-line
- #:program "verify"
  #:once-each
  [("-s" "--start") s
                    "Start invoking the SMT solver beyond this point"